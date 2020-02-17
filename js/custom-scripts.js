2c2c5a907175f263ec37b1a68e62954f8eacada3
DarioGonzalez-Axians <dario.gonzalez@axians.es>
2020-02-17 11:43:11 +0100

    Add error on iframes load

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -1,4 +1,4 @@
-! function(e) {
+! function (e) {
     function a() {
         if (100 < e("#mainNav").offset().top) {
             e("#logo-principal").addClass("show");
@@ -10,3 +10,21 @@
     }
     a(), e(window).scroll(a)
 }(jQuery);
+
+$(document).ready(function () {
+    $.ajax("https://www.instagram.com", {
+        type: "GET",
+    }).fail(function () {
+        $("#exodium-instagram").html('<img id="instagram-error" src="./img/error/instagram-error.png" alt="" class="img-fluid"></img>');
+    });
+    $.ajax("https://www.twitter.com", {
+        type: "GET",
+    }).fail(function () {
+        $("#exodium-twitter").html('<img id="twitter-error" src="./img/error/twitter-error.png" alt="" class="img-fluid"></img>');
+    });
+    $.ajax("https://www.facebook.com", {
+        type: "GET",
+    }).fail(function () {
+        $("#exodium-facebook").html('<img id="facebook-error" src="./img/error/facebook-error.png" alt="" class="img-fluid"></img>');
+    });
+});
